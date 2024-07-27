ff84975bdcad63ae07a66f102782b69f83d254e1
kadismile <ikadismile@gmail.com>
2024-07-27 16:32:05 -0500

    finished up husky setup

--- src/types/queryresults.ts
+++ src/types/queryresults.ts
@@ -1,8 +1,8 @@
 export type AdvancedQueryResult<T> = {
-  results: T[]
-  count: number
+  results: T[];
+  count: number;
   limit: number;
   page: number;
   prevPage: number;
-  nextPage: number
+  nextPage: number;
 };
